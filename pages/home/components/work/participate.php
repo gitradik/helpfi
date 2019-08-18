611ee45ff42363b0efed361fd324105611e171fe
gitradik <rminzp17@gmail.com>
2019-08-18 16:15:56 +0200

    fdsfs

--- pages/home/components/work/participate.php
+++ pages/home/components/work/participate.php
@@ -13,12 +13,63 @@
             <div class="col-md-12">
                 <div class="roadmap">
                     <div class="roadmap-item">
+                        <div class="roadmap-text w-100">
+                            <p>Идея</p>
+                            <p></p>
+                        </div>
+                        <div class="roadmap-icon roadmap-icon-left"><span>2015</span></div>
                         <div></div>
-                        <div class="roadmap-icon"><span>2015</span></div>
+                    </div>
+                    <div class="roadmap-item rmit">
+                        <div></div>
+                        <div class="roadmap-icon"><span>2016</span></div>
                         <div class="roadmap-text">
                             <p>Изучение рынка </p>
                             <p>Создание и регистрация торговой марки HelpFi 09.01.2018</p>
                         </div>
+                    </div>
+                    <div class="roadmap-item rmitb">
+                        <div class="roadmap-text">
+                            <p>Создание рабочего прототипа HelpFi </p>
+                            <p>
+                                проведение тестовой рекламной компании
+
+                                наполнения базы исполнителями</p>
+                        </div>
+                        <div class="roadmap-icon roadmap-icon-left"><span>2017</span></div>
+                        <div></div>
+                    </div>
+                    <div class="roadmap-item rmitb2">
+                        <div></div>
+                        <div class="roadmap-icon"><span>2018</span></div>
+                        <div class="roadmap-text">
+                            <p>Создание рекламных роликов </p>
+                            <p>Привлечение инвесторов</p>
+                        </div>
+                    </div>
+                    <div class="roadmap-item rmitb3">
+                        <div class="roadmap-text">
+                            <p>Доработка проекта и старт рекламной компании  в г.Киев</p>
+                            <p></p>
+                        </div>
+                        <div class="roadmap-icon roadmap-icon-left"><span>2019</span></div>
+                        <div></div>
+                    </div>
+                    <div class="roadmap-item rmitb4">
+                        <div></div>
+                        <div class="roadmap-icon"><span>2109</span></div>
+                        <div class="roadmap-text">
+                            <p>Развитие Хелпфи в г. Киев и получение прибыли </p>
+                            <p></p>
+                        </div>
+                    </div>
+                    <div class="roadmap-item rmitb4">
+                        <div class="roadmap-text">
+                            <p>Развитие хелпфи  в других городах и странах</p>
+                            <p></p>
+                        </div>
+                        <div class="roadmap-icon roadmap-icon-left"><span>2019</span></div>
+                        <div></div>
                     </div>
                 </div>
             </div>
