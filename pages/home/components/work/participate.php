29be0a15e347955aae2cd97244c40c3f330f6070
gitradik <rminzp17@gmail.com>
2019-08-18 16:53:43 +0200

    fdsfs

--- pages/home/components/work/participate.php
+++ pages/home/components/work/participate.php
@@ -12,15 +12,15 @@
             </div>
             <div class="col-md-12">
                 <div class="roadmap">
-                    <div class="roadmap-item">
-                        <div class="roadmap-text w-100">
+                    <div class="roadmap-item" data-aos="fade-right" data-aos-duration="1000">
+                        <div class="roadmap-text order-1 order-lg-0">
                             <p>Идея</p>
                             <p></p>
                         </div>
-                        <div class="roadmap-icon roadmap-icon-left"><span>2015</span></div>
+                        <div class="roadmap-icon roadmap-icon-left order-0 order-lg-1"><span>2015</span></div>
                         <div></div>
                     </div>
-                    <div class="roadmap-item rmit">
+                    <div class="roadmap-item rmit" data-aos="fade-left" data-aos-duration="1000">
                         <div></div>
                         <div class="roadmap-icon"><span>2016</span></div>
                         <div class="roadmap-text">
@@ -28,18 +28,18 @@
                             <p>Создание и регистрация торговой марки HelpFi 09.01.2018</p>
                         </div>
                     </div>
-                    <div class="roadmap-item rmitb">
-                        <div class="roadmap-text">
+                    <div class="roadmap-item rmitb" data-aos="fade-right" data-aos-duration="1000">
+                        <div class="roadmap-text order-1 order-lg-0">
                             <p>Создание рабочего прототипа HelpFi </p>
                             <p>
                                 проведение тестовой рекламной компании
 
                                 наполнения базы исполнителями</p>
                         </div>
-                        <div class="roadmap-icon roadmap-icon-left"><span>2017</span></div>
+                        <div class="roadmap-icon roadmap-icon-left order-0 order-lg-1"><span>2017</span></div>
                         <div></div>
                     </div>
-                    <div class="roadmap-item rmitb2">
+                    <div class="roadmap-item rmitb2" data-aos="fade-left" data-aos-duration="1000">
                         <div></div>
                         <div class="roadmap-icon"><span>2018</span></div>
                         <div class="roadmap-text">
@@ -47,15 +47,15 @@
                             <p>Привлечение инвесторов</p>
                         </div>
                     </div>
-                    <div class="roadmap-item rmitb3">
-                        <div class="roadmap-text">
+                    <div class="roadmap-item rmitb3" data-aos="fade-right" data-aos-duration="1000">
+                        <div class="roadmap-text order-1 order-lg-0">
                             <p>Доработка проекта и старт рекламной компании  в г.Киев</p>
                             <p></p>
                         </div>
-                        <div class="roadmap-icon roadmap-icon-left"><span>2019</span></div>
+                        <div class="roadmap-icon roadmap-icon-left order-0 order-lg-1"><span>2019</span></div>
                         <div></div>
                     </div>
-                    <div class="roadmap-item rmitb4">
+                    <div class="roadmap-item rmitb4" data-aos="fade-left" data-aos-duration="1000">
                         <div></div>
                         <div class="roadmap-icon"><span>2109</span></div>
                         <div class="roadmap-text">
@@ -63,12 +63,12 @@
                             <p></p>
                         </div>
                     </div>
-                    <div class="roadmap-item rmitb4">
-                        <div class="roadmap-text">
+                    <div class="roadmap-item rmitb4" data-aos="fade-right" data-aos-duration="1000">
+                        <div class="roadmap-text order-1 order-lg-0">
                             <p>Развитие хелпфи  в других городах и странах</p>
                             <p></p>
                         </div>
-                        <div class="roadmap-icon roadmap-icon-left"><span>2019</span></div>
+                        <div class="roadmap-icon roadmap-icon-left order-0 order-lg-1"><span>2019</span></div>
                         <div></div>
                     </div>
                 </div>
