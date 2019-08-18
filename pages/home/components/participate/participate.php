278a550420be66eb2bcb98bbf9d38a53f2fb2507
gitradik <rminzp17@gmail.com>
2019-08-18 13:16:20 +0200

    fdsfs

--- pages/home/components/participate/participate.php
+++ pages/home/components/participate/participate.php
@@ -11,12 +11,12 @@
 
                         Можно использовать как два вместе так и по отдельности</h2>
                 </div>
-                <div class="col-md-6">
-
-                </div>
-                <div class="col-md-6">
-
-                </div>
+            </div>
+            <div class="col-md-6">
+                <img src="<?= get_template_directory_uri(); ?>/images/sertificats/1.png" alt="" class="img-fluid">
+            </div>
+            <div class="col-md-6">
+                <img src="<?= get_template_directory_uri(); ?>/images/sertificats/2.png" alt="" class="img-fluid">
             </div>
         </div>
     </div>
